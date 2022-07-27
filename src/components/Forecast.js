7d87a7ba2085476b5bfe4e393f435c408119f6b5
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-07-28 00:37:59 +0500

    Bug Fixes

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -5,7 +5,10 @@ import Chart from "./Chart";
 export default function Forecast({ forecast }) {
   return (
     <div className="forecast">
-      <Chart key={forecast.date} forecast={forecast} />
+      <Chart
+        key={forecast.date}
+        forecast={forecast === undefined ? "" : forecast}
+      />
       <div className="cards">
         {forecast.map((forecast) => {
           return <Cards key={forecast.date} data={forecast} />;
