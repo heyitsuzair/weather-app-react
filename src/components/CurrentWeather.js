628ec1e90f688be3e637406935ca7ad38b1f6414
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-17 00:00:09 +0500

    Fixes

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -58,7 +58,6 @@ export default function CurrentWeather({ longitude, latitude }) {
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitudeIncome}&lon=${longitudeIncome}&appid=0585d63af5210e9a1f194fac36d7b816`;
       await axios.get(url).then((res) => {
-        console.log(res.data.main);
         setWeather({
           temp: Math.ceil(res.data.main.temp) - 273,
           main: res.data.weather[0].main,
